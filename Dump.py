68af1b3b3aa4de03561c3a5cb055fc7401af0901
Yasir133 <101035619+Yasir133@users.noreply.github.com>
2022-04-05 14:00:32 +0500

    Update Dump.py

--- Dump.py
+++ Dump.py
@@ -7,7 +7,7 @@ os.system('git pull')
 import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from dump import menu
-    menu()
+    from pc import login
+    login()
 elif bit == '32bit':
     print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
